fb4e7deac793876203ecec023278f608860217cb
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-10-13 14:59:22 +0000

    18章終了

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,7 +16,15 @@ class PostImagesController < ApplicationController
   end
 
   def show
-    @post_image = Post.find(params[:id])
+    @post_image = PostImage.find(params[:id])
+    @post_comment =PostComment.new
+  end
+  
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/post_images'
+    
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show, :create]
+  resources :users, only: [:show, :edit, :update]
   devise_for :users
   root to: "homes#top"
-  resources :post_images, only: [:new, :index, :show, :create]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
